1e58e93e63d56aebe7e8b793c6016e2e32aef670
RDSMatheus <matheus_rds1998@hotmail.com>
2023-09-06 18:41:20 -0300

    Head adicionado no NotFound

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Snorlax from '../Assets/snorlax-404.svg';
 import styles from './NotFound.module.css';
+import Head from './Head';
 
 const NotFound = () => {
   return (
     <div className={styles.notFound}>
+      <Head description="Página de erro" title="404" />
       <p className={styles.p}>A wild Snorlax appears!</p>
       <p>
         4<img src={Snorlax} alt="Snorlax" />4
